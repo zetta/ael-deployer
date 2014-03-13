85816ec73c9bc1dba81ef670fdc10db1275f9a36
zetta <zetaweb@gmail.com>
2014-03-13 00:46:39 -0600

    again same typo at sprintf

--- src/A3l/Deployer/Notifier/NotifierManager.php
+++ src/A3l/Deployer/Notifier/NotifierManager.php
@@ -59,7 +59,7 @@ $client->send($message);
 
         // Create a message
         $message = \Swift_Message::newInstance(sprintf('Deploy [%s]', $projectName))
-          ->setFrom(array($this->configurator->getConfig()['app']['mail']['username'] => sprintfs('%s via deployer', $username)))
+          ->setFrom(array($this->configurator->getConfig()['app']['mail']['username'] => sprintf('%s via deployer', $username)))
           ->setBody($content)
         ;
 
